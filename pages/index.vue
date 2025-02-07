f8f495f136eaf24c4e71bfa213330e8ec9807922
Carlos Vasquez <willrp2527@gmail.com>
2025-02-07 16:44:26 -0500

    upd router path

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,12 @@
 <template>
-    <div>
-        <h1>Page home</h1>
+  <div>
+    <h1>Page home</h1>
+  </div>
+  <div class="card ">
+    <div class="card-body">
+      <p class="card-text">
+        Example proyect with Nuxt3 VueJS
+          </p>
     </div>
+  </div>
 </template>
--- pages/products-[category]/[title].vue
+++ pages/products-[category]/[title].vue
@@ -1,8 +1,30 @@
 <script lang="ts" setup>
 const route = useRoute();
+definePageMeta({
+  validate: (route) => {
+    const title = route.params.title as string;
+    const regex = /^(?!\d+$)[a-zA-Z0-9-]+$/;
+    return regex.test(title);
+  }
+});
 </script>
 <template>
   <div>
-    <h1>{{ route.params.title }}</h1>
+    <h1></h1>
+    <h2></h2>
+  </div>
+  <div class="card">
+    <img src="..." class="card-img-top" alt="..." />
+    <div class="card-body">
+      <h5 class="card-title text-center">{{ route.params.title }}</h5>
+      <p class="card-text">
+        Some quick example text to build on the card title and make up the bulk
+        of the card's content.
+      </p>
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">Category: {{ route.params.category }}</li>
+      <li class="list-group-item">Computer:{{ route.params.title }}</li>
+    </ul>
   </div>
 </template>
